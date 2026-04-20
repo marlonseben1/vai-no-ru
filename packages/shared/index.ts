2b0b0c9b8dbac0ae8a9673aa02dde7c3751cc222
marlonseben <marlonseben1@gmail.com>
2026-04-19 21:27:26 -0300

    refactor(zod-schema): Faz melhorias no zod schema e adiciona entries dos forms de prod e staging

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -18,16 +18,31 @@ export const reservaDiaSchema = z.object({
   refeicao: refeicaoEnum,
 });
 
-export const ruFormSchema = z.object({
-  email: z.email('E-mail inválido').min(1, 'O e-mail é obrigatório'),
-  nome: z.string().min(3, 'O nome deve conter pelo menos 3 caracteres'),
-  matricula: z
-    .string()
-    .min(1, 'Informe o número da matrícula')
-    .regex(/^\d+$/, 'A matrícula deve conter apenas números'),
-  perfil: perfilEnum,
-  data: z.array(reservaDiaSchema).min(1, 'Você deve informar pelo menos uma data'),
-});
+export const ruFormSchema = z
+  .object({
+    email: z.email('E-mail inválido').min(1, 'O e-mail é obrigatório'),
+    nome: z.string().min(3, 'O nome deve conter pelo menos 3 caracteres'),
+    matricula: z
+      .string()
+      .regex(/^\d*$/, 'A matrícula deve conter apenas números')
+      .optional(),
+    perfil: perfilEnum,
+    data: z
+      .array(reservaDiaSchema)
+      .min(1, 'Você deve informar pelo menos uma data'),
+  })
+  .superRefine((data, ctx) => {
+    if (
+      data.perfil === 'Aluno graduação UPF' &&
+      (!data.matricula || data.matricula.trim() === '')
+    ) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Informe o número da matrícula',
+        path: ['matricula'],
+      });
+    }
+  });
 
 export type RuUpfData = z.infer<typeof ruFormSchema>;
 
@@ -37,4 +52,19 @@ export const GOOGLE_ENTRIES = {
   REFEICAO: 'entry.24359638',
   PERFIL: 'entry.919059448',
   DATA: 'entry.438994047', // _year, _month, _day
+  PROD: {
+    NOME: 'entry.1773563523',
+    MATRICULA: 'entry.1642444040',
+    REFEICAO: 'entry.24359638',
+    PERFIL: 'entry.919059448',
+    DATA: 'entry.438994047', // _year, _month, _day
+  },
+  STAGING: {
+    EMAIL: 'entry.1492417277',
+    DATA: 'entry.1801437263',
+    REFEICAO: 'entry.1988784789',
+    NOME: 'entry.1076655601',
+    PERFIL: 'entry.336191021',
+    MATRICULA: 'entry.1251347209',
+  },
 } as const;
